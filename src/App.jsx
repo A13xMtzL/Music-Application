fc944b5d5f7653c28bdc473d3ac0ebf0324fb8d4
Alejandro Martínez Luna <A01276785@tec.mx>
2024-03-05 15:40:51 -0600

    Refactor code structure and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import './App.css'
-import { NavBar } from './components/Navbar'
 import { CourseOverview } from './components/CourseOverview'
+import { NavBar } from './components/Navbar'
 import PreviewCourse from './components/PreviewCourse'
+import WhoAmI from './components/WhoAmI'
 
 function App() {
-
   return (
     <>
-      <div className='flex'>
+      <div className='flex justify-between items-center'>
         <NavBar />
-        {/* <SideMenu/> */}
-        <CourseOverview />
-
-        <PreviewCourse/>
+        <div className='flex justify-between '>
+          {/* Elemento a la izquierda */}
+          <div className='flex flex-col w-1/5  justify-end'>
+            <WhoAmI />
+          </div>
+          {/* Elemento del centro, quiero que este abarque todo el archivo */}
+          <div className='flex-grow w-1/2'>
+            <CourseOverview />
+          </div>
+          {/* Elemento de la derecha */}
+          <div className=''>
+            <PreviewCourse />
+          </div>
+        </div>
       </div>
     </>
   )
