05c86acc39d9d6cb5badb1ec89fb1222b3b30259
Alejandro Martínez Luna <A01276785@tec.mx>
2024-03-10 16:16:31 -0600

    Add guitarCourse object to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,22 @@ import HomePage from './pages/HomePage';
 import ConditionalFooter from './components/layout/ConditionalFooter';
 
 function App() {
+
+  const guitarCourse = {
+    title: 'Guitarra',
+    description: 'En este curso de guitarra, aprenderás desde los fundamentos básicos hasta técnicas avanzadas para mejorar tu habilidad en el instrumento. Exploraremos una variedad de acordes, patrones de rasgueo y técnicas de improvisación para ayudarte a expresarte musicalmente de manera creativa. Con lecciones dinámicas y divertidas, te guiaremos en tu viaje para convertirte en un guitarrista versátil y seguro.',
+    // videoId: '1g0DoZH6mXTX-Dim3i1bp9FZc9ASgd4T3'
+    videoId : '1g0DoZH6mXTX-Dim3i1bp9FZc9ASgd4T3' // Reemplaza esto con tu ID de video de Google Drive
+
+  }
+
   return (
     <>
       <Router>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />{/*---> Se cambia por el archivo de inicio */}
-          <Route path="/guitar" element={<CoursePage />} />
+          <Route path="/guitar" element={<CoursePage course={guitarCourse} />} />
           <Route path="/courses" element={<AllCourses />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/faq" element={<FAQPage />} />
